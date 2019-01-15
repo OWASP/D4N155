3faca4702071c5847693aa726ca02905972e68eb
jul10l1r4 <jul10l1r4@ufrn.edu.br>
2019-01-15 03:09:46 +0100

    Fix problems breaking the MAIN!

--- modules/generator.py
+++ modules/generator.py
@@ -1,6 +1,13 @@
+# It module make the word list
 import sys
 import string
 
+# Function
+def counting (x):
+    return x+"123"
+
 texts = [texts.translate(str.maketrans('','', string.punctuation)) for texts in sys.argv[1].split()]
-print('\n'.join(set(texts)))
+text123 = map(counting,texts)
+print(text123)
+#print('\n'.join(set(texts)))
 
