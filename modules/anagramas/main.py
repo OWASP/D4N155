55d00ee12466526b0943f4bd5b50946681aa9ccb
jul10l1r4 <jul10l1r4@ufrn.edu.br>
2019-01-18 06:59:27 +0100

    hackernization are done

--- modules/anagramas/main.py
+++ modules/anagramas/main.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 import sys
 
-# check if are lowcase or no
+# It code are a test
+
 def hackableMenuscula (x):
-    blob = x.replace('a','4')
+    blob = x.lower()
+
+    # Replace vog.
+    blob = blob.replace('a','4')
     blob = blob.replace('e','3')
     blob = blob.replace('i','1')
     blob = blob.replace('o','0')
+    
+    # Replace con.
+    blob = blob.replace('t','7')
     return blob
 
 out = list(map(hackableMenuscula,sys.argv[1]))
