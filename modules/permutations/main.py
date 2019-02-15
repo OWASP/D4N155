ff77d2fd1dbf7ea5980576f570b665107ffb6424
Matheus  Oliveira <37408291+matholiveira91@users.noreply.github.com>
2019-02-15 19:19:28 -0300

    eliminate bad whitespaces

--- modules/permutations/main.py
+++ modules/permutations/main.py
@@ -3,40 +3,40 @@
 # of variables
 global_list = []
 # Texts to numbers 
-def textnumber (x):
+def textnumber(x):
     return x.translate(str.maketrans('aeioAEIOBTbt','431043108787'))
 
 # make the number
-def counting1 (x):
+def counting1(x):
     return x+"1"
-def counting2 (x):
+def counting2(x):
     return x+"12"
-def counting3 (x):
+def counting3(x):
     return x+"123"
-def counting4 (x):
+def counting4(x):
     return x+"1234"
-def counting8 (x):
+def counting8(x):
     return x+"12345678"
-def counting81 (x):
+def counting81(x):
     return x+"87654321"
 # Weaks year
-def year9 (x):
+def year9(x):
     return x+"2019"
-def year8 (x):
+def year8(x):
     return x+"2018"
-def year7 (x):
+def year7(x):
     return x+"2017"
-def year6 (x):
+def year6(x):
     return x+"2016"
 
 # Variant, all uppercase or lowercase
 # Up, Low, and upper lower lower up
 # and inverter
-def uppall (x):
+def uppall(x):
     return x.upper()
-def lowera (x):
+def lowera(x):
     return x.lower()
-def ullual (x):
+def ullual(x):
     # k = key
     # k[0] -> position
     # k[1] -> value
@@ -49,9 +49,9 @@ def ullual (x):
             )
         )
 
-def luulal (x):
+def luulal(x):
     return ullual(x).swapcase()
-def inverter (x):
+def inverter(x):
     return x.swapcase()
 
 # Combinatories analysis
@@ -61,7 +61,7 @@ def comb(allist):
 
     # Get value [[1,2,3],[3,2,1]] to
     # parse [1,2,3,3,2,1] 
-    def parsing (x):
+    def parsing(x):
         global global_list
         for i in x:
             global_list = global_list + i
