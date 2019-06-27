7738d95ec3feff0b216dcd50a887529cdbad664e
jul10l14 <jul10l1r4@ufrn.edu.br>
2019-06-27 03:13:10 -0300

    Make API version of generator

--- modules/generator.py
+++ modules/generator.py
@@ -6,51 +6,50 @@ details = False
 
 
 # Root text
-texts = open(sys.argv[1], 'r').read().split()
-new = []
-# Alternations
-texts = texts+list(map(inverter, texts))
-pool = ThreadPool(2)
-def run(fuction,value):
-    # Thread will be parameter type flag
-    # --------------------| All word > 3 and < 30
-    return list(filter(lambda x: True if (len(x) > 3 and len(x) < 30) else False, list(pool.map(fuction, value))))
+#texts = open(sys.argv[1], 'r').read().split()
+def main(texts):
+    # Alternations
+    texts = [texts]+list(map(inverter, texts))
+    pool = ThreadPool(2)
+    def run(fuction,value):
+        # Thread will be parameter type flag
+        # --------------------| All word > 3 and < 30
+        return list(filter(lambda x: True if (len(x) > 3 and len(x) < 30) else False, list(pool.map(fuction, value))))
 
-# Main
-def make():
-    #   EX
-    # counting 3: → x+"123"
-    blob = run(counting1, texts)
-    blob = blob + run(counting2, texts)
-    blob = blob + run(counting3, texts)
-    blob = blob + run(counting4, texts)
-    blob = blob + run(counting8, texts)
-    blob = blob + run(counting81, texts)
-    #   EX
-    # year9 : → x+"2019"
-    blob = blob + run(year9, texts)
-    blob = blob + run(year8, texts)
-    blob = blob + run(year7, texts)
-    blob = blob + run(year6, texts)
-    # EX
-    # YEAR : → "Y34R"
-    blob = blob + run(textnumber, texts)
-    # EX 
-    # year : → "YeAr"
-    blob = blob + run(ullual, texts)
-    blob = blob + run(luulal, texts)
-    blob = blob + run(inverter, texts)
-    blob = blob + run(uppall, texts)
-    blob = blob + run(lowera, texts)
-    # EX
-    # New Yeah : → "YeahNew"
-    blob = blob + comb(texts)
-    
-    # Are finals test details
-    if details == True:
-        blob = blob + pool.comb(blob)
+    # Main
+    def make():
+        #   EX
+        # counting 3: → x+"123"
+        blob = run(counting1, texts)
+        blob = blob + run(counting2, texts)
+        blob = blob + run(counting3, texts)
+        blob = blob + run(counting4, texts)
+        blob = blob + run(counting8, texts)
+        blob = blob + run(counting81, texts)
+        #   EX
+        # year9 : → x+"2019"
+        blob = blob + run(year9, texts)
+        blob = blob + run(year8, texts)
+        blob = blob + run(year7, texts)
+        blob = blob + run(year6, texts)
+        # EX
+        # YEAR : → "Y34R"
+        blob = blob + run(textnumber, texts)
+        # EX 
+        # year : → "YeAr"
+        blob = blob + run(ullual, texts)
+        blob = blob + run(luulal, texts)
+        blob = blob + run(inverter, texts)
+        blob = blob + run(uppall, texts)
+        blob = blob + run(lowera, texts)
+        # EX
+        # New Yeah : → "YeahNew"
+        blob = blob + comb(texts)
+        
+        # Are finals test details
+        if details == True:
+            blob = blob + pool.comb(blob)
 
-    return blob
-
-# Reduce all output for best precision
-print('\n'.join(set(texts+make())))
+        return blob
+    # Reduce all output for best precision
+    return '\n'.join(set(texts+make()))
