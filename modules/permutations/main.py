814077fe8ffa831c4da85f52119e5d6b0913c327
jul10l1r4 <jul10l1r4@ap.router>
2019-08-18 11:09:43 -0300

    :dog: Removing function \"comb\" of permutation module

--- modules/permutations/main.py
+++ modules/permutations/main.py
@@ -51,17 +51,3 @@ def luulal(x):
     return ullual(x).swapcase()
 def inverter(x):
     return x.swapcase()
-# Combinatories analysis
-# comb -> mess & ssem -> parsing
-# returning all parsing strings
-def comb(allist):
-    # Get value [[1,2,3],[3,2,1]] to
-    # parse [1,2,3,3,2,1] 
-    def parsing(x):
-        global global_list
-        for i in x:
-            global_list = global_list + i
-        return global_list
-
-    # Mergeds values
-    return parsing(list(map(lambda x: [x+i for i in allist], allist)))
