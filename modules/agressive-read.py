5b5b9be2e19b897ba3dca10a402d3e3b7316dab3
jul10l1r4 <jul10l1r4@disroot.org>
2019-12-30 20:25:38 -0300

    Read with handless

---
+++ modules/agressive-read.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import sys
+import unittest, time, re
+from selenium import webdriver
+
+# Select motor
+driver = webdriver.Firefox(executable_path='./modules/geckodriver/geckodriver')
+driver.get(sys.argv[1]) # Arg: url
+# Show all texts
+print(driver.find_element_by_tag_name('body').text)
