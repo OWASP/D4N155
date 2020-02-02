5d9246eedd00b9d376b0f4631c83aab9f2014479
jul10l1r4 <jul10l1r4@disroot.org>
2020-02-02 13:16:35 -0300

    :tada: Add domain function, msg and fix make function

--- app.py
+++ app.py
@@ -1,30 +1,34 @@
 from flask import Flask
 from flask import jsonify
 import os
+os.sys.path.append('getrails/')
+from getrails.search import search_now
 os.sys.path.append('modules/')
 from generator import main
 
 port = int(os.environ.get("PORT", 5000))
 app = Flask(__name__)
+msg = "Its OWASP D4N155 project for API, see: https://github.com/OWASP/D4N155, branch: api"
     
-# See all report
+# See all data
 @app.route('/')
 def index():
     # Get all registers in DB
-    return jsonify(result = "Get all registers")
-# See report {id}
-@app.route('/<param>')
-def show(param):
+    return jsonify(result = "Get all data", helpus = "{}".format(msg))
+
+# See all urls of domain
+@app.route('/domain/<param>')
+def domain(param):
     # Get id in DB
-    return jsonify(result = "Get register based in id: {}".format(param))
-    del param
+    get_urls = search_now("site:{}".format(param))
+    response = jsonify(result = get_urls, helpus = "{}".format(msg))
+    return response
 
 # Make new analyze
-@app.route('/gen/<param>')
-def gen(param):
-    # Registerj in DB
-    resp = jsonify(result = main(param))
-    return resp
+@app.route('/make/<param>')
+def make(param):
+    response = jsonify(result = main(param), helpus = "{}".format(msg))
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0', port=port)
