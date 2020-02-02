c7e2905c1dd3b12876e517a4a821cd09cb5a0c38
jul10l1r4 <jul10l1r4@disroot.org>
2020-02-02 13:18:34 -0300

    :tada: Functionly objetive.py

--- objetive/objetive.py
+++ objetive/objetive.py
@@ -1,39 +1,38 @@
-# -*- coding: utf-8 -*-
-#from func_timeout import func_timeout, FunctionTimedOut
 import sys
 import mechanicalsoup # needed
 
-args = 0
-# Connect to target
-browser = mechanicalsoup.StatefulBrowser()
+def main(url):
+    # Connect to target
+    browser = mechanicalsoup.StatefulBrowser()
 
-try:
-    status = browser.open(sys.argv[1]) # func_timeout(15, browser.open, args=sys.argv[1])
+    try:
+        status = browser.open(url) # func_timeout(15, browser.open, args=sys.argv[1])
 
-    if 'text/' in status.headers['Content-Type']:
+        if 'text/' in status.headers['Content-Type']:
+            pass
+        else:
+            #exit(2)
+            pass
+
+    except:
+        #exit(1)
         pass
-    else:
-        exit(2)
 
-except:
-    exit(1)
+    # All functions for get values
+    def all():
+        value = ""
+        for p in browser.get_current_page().select('p'):
+            value = value + p.text + ' '
 
-# All functions for get values
-def all():
-    value = ""
-    for p in browser.get_current_page().select('p'):
-        value = value + p.text + ' '
+        for h1 in browser.get_current_page().select('h1'):
+            value = value + h1.text + ' '
 
-    for h1 in browser.get_current_page().select('h1'):
-        value = value + h1.text + ' '
+        for link in browser.get_current_page().select('a'):
+            value = value + link.text + ' '
 
-    for link in browser.get_current_page().select('a'):
-        value = value + link.text + ' '
-
-    if value:
         return value.translate(str.maketrans('', '','–,'))
-    else:
-        exit(2)
-
-# All texts
-print(all())
+    # All texts
+    try:
+        return all()
+    except:
+        return "Not any texts :("
