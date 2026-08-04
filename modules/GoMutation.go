c022e413698930bf52e184db74aa57359119da0f
jul10l1r4 <jul10l1r4@disroot.org>
2026-08-04 15:51:23 -0300

    :pick: Refactor: stream-based mutation generator with incremental dedupe

--- modules/GoMutation.go
+++ modules/GoMutation.go
@@ -1,127 +1,155 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"strings"
-	"io/ioutil"
 	"os"
+	"strings"
 	"strconv"
 )
 
-// Check if err
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
 
-// Removing duplicated items
-func unique(intSlice []string) []string {
-    keys := make(map[string]bool)
-    list := []string {}
-    for _, entry := range intSlice {
-        if _, value := keys[entry]; !value {
-            keys[entry] = true
-            list = append(list, entry)
-        }
-    }
-    return list
-}
-
-// Pure functions
 func leet(word string) string {
-	return strings.NewReplacer("A","4", "E", "3", "I", "1", "O", "0", "S", "5", "T", "7", "B", "8",
-                                    "a","4", "e", "3", "i", "1", "o", "0", "s", "5", "t", "7", "b", "8").Replace(word)
+	return strings.NewReplacer(
+		"A", "4", "E", "3", "I", "1", "O", "0", "S", "5", "T", "7", "B", "8",
+		"a", "4", "e", "3", "i", "1", "o", "0", "s", "5", "t", "7", "b", "8",
+	).Replace(word)
 }
+
 func count1to8(word string) []string {
-        list := []string {word}
-        for i := 0; i < 9; i++ {
-                list = append(list,list[len(list)-1]+strconv.Itoa(i))
-        }
-        return list
+	list := []string{word}
+	for i := 0; i < 9; i++ {
+		list = append(list, list[len(list)-1]+strconv.Itoa(i))
+	}
+	return list
 }
+
 func year90(word string) []string {
-        list := []string {word}
-        for i := 99; i > 89; i-- {
-                list = append(list,word+strconv.Itoa(i))
-        }
-        return list
+	list := []string{word}
+	for i := 99; i > 89; i-- {
+		list = append(list, word+strconv.Itoa(i))
+	}
+	return list
 }
+
 func year2000(word string) []string {
-        list := []string {word}
-        for i := 2024; i > 1999; i-- {
-                list = append(list,word+strconv.Itoa(i))
-        }
-        return list
+	list := []string{word}
+	for i := 2026; i > 1999; i-- {
+		list = append(list, word+strconv.Itoa(i))
+	}
+	return list
 }
+
 func swapCase(r rune) rune {
-    switch {
-    case 'a' <= r && r <= 'z':
-        return r - 'a' + 'A'
-    case 'A' <= r && r <= 'Z':
-        return r - 'A' + 'a'
-    default:
-        return r
-    }
+	switch {
+	case 'a' <= r && r <= 'z':
+		return r - 'a' + 'A'
+	case 'A' <= r && r <= 'Z':
+		return r - 'A' + 'a'
+	default:
+		return r
+	}
 }
+
 func inverter(word string) string {
-	runes := []rune(string(word))
+	runes := []rune(word)
 	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
 		runes[i], runes[j] = runes[j], runes[i]
 	}
 	return string(runes)
 }
-func mess(word string, allList []string) []string {
-	list := []string {}
-	for _, i := range allList {
-		list = append(list, word + i)
-	}
-	return list
-}
-func combine(allList []string) []string {
-	finalWords := []string {}
 
-	for _, i := range allList {
-		finalWords = append(finalWords, mess(i, allList)...)
+func combine(list []string) []string {
+	final := make([]string, 0, len(list)*len(list))
+	for _, a := range list {
+		for _, b := range list {
+			final = append(final, a+b)
+		}
 	}
-
-	return finalWords
+	return final
 }
 
 func main() {
-	finalContent := []string {}
-	// Read base file
-	blob, err := ioutil.ReadFile(os.Args[1])
+	if len(os.Args) < 3 {
+		fmt.Println("Uso: GoMutation <input-file> <output-file>")
+		os.Exit(1)
+	}
+	inputPath := os.Args[1]
+	outputPath := os.Args[2]
+
+	inFile, err := os.Open(inputPath)
 	check(err)
-	content := strings.Fields(string(blob))
+	defer inFile.Close()
 
-  // Combinatory expression out context of loop. But work finalContent
-	finalContent = append(content, combine(content)...)
+	scanner := bufio.NewScanner(inFile)
+	scanner.Split(bufio.ScanWords)
 
-  // Leet after input in loop
-  for _, word:= range finalContent {
-    content = append(finalContent, leet(word))
-  }
+	content := []string{}
+	for scanner.Scan() {
+		t := scanner.Text()
+		if t != "" {
+			content = append(content, t)
+		}
+	}
+	check(scanner.Err())
 
-	// Running functions for make wordlist
-	for _, word := range content {
-		finalContent = append(finalContent, count1to8(word)...)
-		finalContent = append(finalContent, year90(word)...)
-		finalContent = append(finalContent, year2000(word)...)
-		finalContent = append(finalContent, strings.ToUpper(word))
-		finalContent = append(finalContent, strings.ToLower(word))
-		finalContent = append(finalContent, strings.Map(swapCase, word))
-		finalContent = append(finalContent, inverter(word))
+	outFile, err := os.Create(outputPath)
+	check(err)
+	defer outFile.Close()
+
+	writer := bufio.NewWriter(outFile)
+	defer func() {
+		check(writer.Flush())
+	}()
+
+	seen := make(map[string]struct{})
+
+	emit := func(s string) {
+		if s == "" {
+			return
+		}
+		if _, ok := seen[s]; ok {
+			return
+		}
+		seen[s] = struct{}{}
+		_, err := writer.WriteString(s + "\n")
+		check(err)
 	}
 
-	// Save data
-	file, errCreate := os.Create(os.Args[2])
-	check(errCreate)
-	defer file.Close()
-	data := []byte(strings.Join(unique(finalContent), "\n"))
-	numberWrote, errWrite := file.Write(data)
-	check(errWrite)
-	if numberWrote < 1 {
-		fmt.Println("1b?")
+	processWordVariants := func(word string) {
+		emit(word)
+		emit(leet(word))
+
+		for _, v := range count1to8(word) {
+			emit(v)
+		}
+		for _, v := range year90(word) {
+			emit(v)
+		}
+		for _, v := range year2000(word) {
+			emit(v)
+		}
+
+		emit(strings.ToUpper(word))
+		emit(strings.ToLower(word))
+		emit(strings.Map(swapCase, word))
+		emit(inverter(word))
 	}
+
+	for _, w := range content {
+		processWordVariants(w)
+	}
+
+	for i := 0; i < len(content); i++ {
+		for j := 0; j < len(content); j++ {
+			comb := content[i] + content[j]
+			processWordVariants(comb)
+		}
+	}
+
 }
